dda6497c92d3b13981a8c3bf88a596943a7204ca
Simonas Tamkevicius <simonas.ta@icloud.com>
2023-08-17 23:25:17 -0400

    ready for deploy fixed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: ["https://ecommerce-bead-store-backend.onrender.com", "http://localhost:9000"]
+    baseURL: "https://ecommerce-bead-store-backend.onrender.com"
 });
