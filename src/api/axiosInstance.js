f4123b05fc7bcbe6b4fe3bffc8c0777d79005305
Simonas Tamkevicius <simonas.ta@icloud.com>
2024-06-23 21:29:40 -0400

    changed backend url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const backendBaseURL = "https://ecommerce-bead-store-backend.onrender.com";
+const backendBaseURL = "https://ecommerce-bead-store-backend.onrender.com";
 // const backendBaseURL = "http://localhost:9000";
-const backendBaseURL = "https://fantastic-pink-frog.cyclic.cloud";
+// const backendBaseURL = "https://fantastic-pink-frog.cyclic.cloud";
 
 const axiosInstance = axios.create({
     baseURL: backendBaseURL,
